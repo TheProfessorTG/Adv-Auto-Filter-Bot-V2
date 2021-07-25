8b322a94731b59c65c916eceb61b1f7a1015e091
TheProfessorTG <85480544+TheProfessorTG@users.noreply.github.com>
2021-07-25 11:29:49 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1608,7 +1608,7 @@ async def cb_about(bot, update: CallbackQuery):
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                    "My Dev ⚡", url="https://t.me/JonasKhanwaldTG"
                 ),
                 
             InlineKeyboardButton
@@ -1639,10 +1639,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/JonasKhanwaldTG),
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/JonasKhanwaldTG'),
             InlineKeyboardButton('Source Code 🧾', url ='https://github.com/TheProfessorTG/Adv-Filter-Bot-V2')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/JonasKhanwaldTG)
+            InlineKeyboardButton('Support 🛠', url='https://t.me/JonasKhanwaldTG')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
